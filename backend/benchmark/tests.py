9811108edcf01e2ac4a15d0984f6cdecf2d7cfce
Cristian Suárez <cristiansuarezg7@gmail.com>
2023-02-28 16:54:33 +0100

    Tests and docker setup

--- backend/benchmark/tests.py
+++ backend/benchmark/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from optimize.optimize import efficient, naive
+from .compute import compute_stats
+import time
 
-# Create your tests here.
+
+class BenchmarkViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.num_lists = 3
+        self.num_elements = 5
+        self.replications = 2
+
+    def _test_benchmark_results(self, algorithm):
+        result = compute_stats(algorithm, self.num_lists,
+                               self.num_elements, self.replications, time.time())
+
+        for key, value in result.items():
+            if key == "algorithm":
+                self.assertIsInstance(value, str)
+
+            elif key == "num_lists" or key == "num_elements" or key == "replications" or key == "complexity":
+                self.assertIsInstance(value, int)
+
+            else:
+                self.assertIsInstance(value, float)
+
+    def test_benchmark_efficient(self):
+        self._test_benchmark_results(efficient)
+
+    def test_benchmark_naive(self):
+        self._test_benchmark_results(naive)
--- backend/optimize/tests.py
+++ backend/optimize/tests.py
@@ -1,39 +1,23 @@
-import json
-from django.test import TestCase, Client
-import logging
-
-logger = logging.getLogger('my_logger')
+from django.test import TestCase
+from .optimize import efficient, naive
 
 
 class OptimizeViewTests(TestCase):
 
     def setUp(self):
-        self.client = Client()
         self.lists = [
             [5, 4], [7, 8, 9], [5, 7, 8, 9, 10]
         ]
         self.m = 40
-        self.f = "lambda x: x ** 2"
+        self.f = lambda x: x ** 2
 
-    def _test_optimize(self, url):
-        response = self.client.post(
-            "/optimize" + url,
-            {
-                "lists": [",".join(str(x) for x in lst) for lst in self.lists],
-                "m": self.m,
-                "f": self.f
-            }
-        )
-        logger.debug("Response " + url + " " +
-                     response.content.decode("utf-8"))
+    def _test_optimize(self, algorithm):
+        result = algorithm(self.lists, self.m, self.f)
 
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_data["result"], 37)
-        self.assertIsInstance(response_data["time"], float)
+        self.assertEqual(result, 37)
 
     def test_optimize_efficient(self):
-        self._test_optimize("/optimize_efficient/")
+        self._test_optimize(efficient)
 
     def test_optimize_naive(self):
-        self._test_optimize("/optimize_naive/")
+        self._test_optimize(naive)
